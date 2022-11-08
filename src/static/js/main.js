e0ce50494713715ed777295faa3efbd7b9e04884
Dev GM <117660981+devgmu@users.noreply.github.com>
2022-11-08 13:29:20 +0000

    Added meta settings to index.html and updated default layout

--- src/static/js/main.js
+++ src/static/js/main.js
@@ -1,4 +1,36 @@
-/*
+ //Javascript to toggle the menu
+document.getElementById('nav-toggle').onclick = function () {
+    document.getElementById("nav-content").classList.toggle("hidden");
+  }
+
+/*  window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 400 || document.documentElement.scrollTop > 400) {
+    document.getElementById("navbar").style.padding = "4px 15px";
+    document.getElementById("navbar").style.opacity = "1";
+
+   
+  } else {
+    document.getElementById("navbar").style.padding = "15px 15px";
+
+  }
+}
+  
+  //* Menu on scroll
+  /*var prevScrollpos = window.pageYOffset;
+  window.onscroll = function() {
+  var currentScrollPos = window.pageYOffset;
+    if (prevScrollpos > currentScrollPos) {
+      document.getElementById("navbar").style.top = "0";
+      document.getElementById("navbar").style.opacity = "3";
+    } else {
+      document.getElementById("navbar").style.top = "-50px";
+      document.getElementById("navbar").style.opacity = "0";
+    }
+    prevScrollpos = currentScrollPos;
+  }
+ /*
    * Light YouTube Embeds by @labnol
    * Credit: https://www.labnol.org/
    */
