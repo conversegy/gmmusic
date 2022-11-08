bf860e0884fd18e9f6dc86fd0acaa8dde0057a81
Dev GM <117660981+devgmu@users.noreply.github.com>
2022-11-08 10:18:12 +0000

    Added Syne font and tried to get toggle working as not working locally

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+
 module.exports = {
   content: ["./**/*.html"],
   theme: {
+    screens: {
+      xs: "314px",
+      sm: '480px',
+      md: '786px',
+      lg: '1024px',
+      xl: '1200px',
+    },
     container: {
       center: true,
     },
-    fontFamily: {
-      'syne': ['Syne Mono'],
-    },
     extend: {
       colors: {},
+      fontFamily: {
+        'syne': ['Syne Mono', 'monospace'],
+      },
     },
   },
   variants: {},
